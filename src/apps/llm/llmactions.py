e428b759e2cca62ea09c48a77edba727bcdbe8fa
techatlasdev <srgoqui@gmail.com>
2024-11-15 22:52:01 -0500

    Añadiendo verbosidad a las acciones

--- src/apps/llm/llmactions.py
+++ src/apps/llm/llmactions.py
@@ -1,4 +1,5 @@
 # Funcion que sirve para controlar acciones que se reciban
+from asyncio import sleep
 from pyrogram import Client, filters
 from pyrogram.types.bots_and_keyboards.callback_query import CallbackQuery
 from pyrogram.types.messages_and_media.message import Message
@@ -23,7 +24,10 @@ async def main(client:Client, CallbackQuery:CallbackQuery, postdata=0):
 @Client.on_message(filters.command("clear"))
 async def clear(client:Client, message:Message):
   if message.from_user.id:
+    sticker_loading = await client.send_sticker(message.chat.id, "CAACAgIAAxkBAAENIfZnOBR8ghBQF2ksQk-vFX3XVXeDMwACSQIAAladvQoqlwydCFMhDjYE")
     CHAT_USER_ACTIONS(message).deleteChat(message.from_user.id)
-    await message.reply_text("[⚠️] LA MEMORIA DE CUVO ESTÁ LIMPIA.")
+    await sticker_loading.delete()
+    await sleep(1)
+    await message.reply_text("[✅] LA MEMORIA DE CUVO ESTÁ LIMPIA.")
   else:
     await message.reply_text(f"Oye 😡 no permitiré que me borres la memoria, me lo ordenó mi creador.")
