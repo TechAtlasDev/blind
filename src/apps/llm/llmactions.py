ebc32958a49398c22d8e84e21167eaab3b0c71d2
techatlasdev <srgoqui@gmail.com>
2024-11-15 22:38:50 -0500

    Añadiendo verbosidad a las acciones

--- src/apps/llm/llmactions.py
+++ src/apps/llm/llmactions.py
@@ -21,7 +21,7 @@ async def main(client:Client, CallbackQuery:CallbackQuery, postdata=0):
 
 @Client.on_message(filters.command("clear"))
 async def clear(client:Client, message):
-  if message.from_user.id == ADMIN_ID:
+  if CallbackQuery.from_user.id:
     CHAT_USER_ACTIONS(message).deleteChat(message.from_user.id)
     await message.reply_text("[⚠️] LA MEMORIA DE CUVO ESTÁ LIMPIA.")
   else:
