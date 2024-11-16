95f75648155bcbe435ddae5ab694792d9978155f
techatlasdev <srgoqui@gmail.com>
2024-11-15 20:39:37 -0500

    Acutaliacion de prueba

--- manager.py
+++ manager.py
@@ -38,7 +38,7 @@ def check_repo_updates(interval=30):
             print("Repositorio actualizado. Reiniciando bot...")
             reload_handler.start_bot()
         else:
-            print ("[INFO-SYSTEM] Sistema actualizado")
+            print ("[INFO-SYSTEM 1] Sistema actualizado")
         time.sleep(interval)
 
 if __name__ == "__main__":
