59be494c8d76d9b48dd03394817cb27e78c7c24d
techatlasdev <srgoqui@gmail.com>
2024-11-15 22:40:21 -0500

    Añadiendo verbosidad a las acciones

--- src/apps/llm/llmactions.py
+++ src/apps/llm/llmactions.py
@@ -1,6 +1,7 @@
 # Funcion que sirve para controlar acciones que se reciban
 from pyrogram import Client, filters
 from pyrogram.types.bots_and_keyboards.callback_query import CallbackQuery
+from pyrogram.types.messages_and_media.message import Message
 #from src.apps.llm.lib.objects import AICUVO
 
 from src.utils.vars import ADMIN_ID
@@ -20,8 +21,8 @@ async def main(client:Client, CallbackQuery:CallbackQuery, postdata=0):
     await CallbackQuery.answer(f"No se encontró un comando cuyo nombre sea: {postdata}")
 
 @Client.on_message(filters.command("clear"))
-async def clear(client:Client, message):
-  if CallbackQuery.from_user.id:
+async def clear(client:Client, message:Message):
+  if message.from_user.id:
     CHAT_USER_ACTIONS(message).deleteChat(message.from_user.id)
     await message.reply_text("[⚠️] LA MEMORIA DE CUVO ESTÁ LIMPIA.")
   else:
