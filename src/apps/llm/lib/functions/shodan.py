b01bfd24d22565aec2d147d45c74357cf00f5986
techatlasdev <srgoqui@gmail.com>
2024-12-03 18:00:21 -0500

    solucionando bugs

--- src/apps/llm/lib/functions/shodan.py
+++ src/apps/llm/lib/functions/shodan.py
@@ -18,8 +18,10 @@ async def searchShodan(query:str, facet:str, limit:int, **kwargs):
   message:Message = kwargs.get("message", None)
   client:Client = kwargs.get("client", None)
 
+  print (query, facet, limit)
+
   try:
-    response = requests.post(f"https://prl412-web-backend.vercel.app/api/search", data=json.dumps({"query": query, "facet": facet, "limit": limit})).json()
+    response = requests.post(f"https://prl412-web-backend.vercel.app/api/search", data={"query": query, "facets": facet, "limit": limit}).json()
     await message.reply_text(f"[💻] <b>CuVo</b> está usando Shodan: <code>{query}</code>.")
     return {"results": response}
   except Exception as e:
